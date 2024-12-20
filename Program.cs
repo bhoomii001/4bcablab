b4faf4cb6066436b0bc95cbdd9ba68bda40b00e9
Bhoomi <bhoomiahir321@gmail.conm>
2024-12-20 09:01:51 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4bcablab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("hello");
+            Console.Write("bhoomi dangar");
+            Console.Read();
+        }
+    }
+}
